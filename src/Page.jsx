9fd7e97e4e0cf2c312e9d551875211b419076e03
Tammyboy23 <fagboyeguntamilore43@gmail.com>
2026-05-05 02:26:57 +0100

    Updated API jey working noe

--- src/Page.jsx
+++ src/Page.jsx
@@ -3,9 +3,9 @@ import { useEffect } from "react";
 function Page(){
     useEffect(() => {
         const get = async() => {
-            const API_KEY = process.env.REACR_APP_API_KEY;
+            const API_KEY = "247b726c211c47eba644541d21179282";
             const res = await
-            fetch(`https://newsapi.org/v2/top-headlines?country=us&${API_KEY}=`)
+            fetch(`https://newsapi.org/v2/everything?q=tesla&from=2026-04-05&sortBy=publishedAt&apiKey=${API_KEY}`)
             const data = await res.json();
             console.log(data);
         }
