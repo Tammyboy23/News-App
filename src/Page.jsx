7706f8b143f8f4221e088828ba11080d6f08e538
Tammyboy23 <fagboyeguntamilore43@gmail.com>
2026-05-05 03:10:25 +0100

    Now u can search for different news as well

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,20 +1,29 @@
 import { useEffect, useState } from "react";
 
 function Page(){
-    const [news, setnews] = useState([])
+    const [news, setnews] = useState([]);
+    const [search, setsearch] = useState("");
     useEffect(() => {
         const get = async() => {
             const API_KEY = import.meta.env.VITE_API_KEY;
+            let some;
+            if(search){
+                some = `everything?q=${search}`;
+            }
+            else{
+                some = `top-headlines?country=us`
+            }
             const res = await
-            fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+            fetch(`https://newsapi.org/v2/${some}&apiKey=${API_KEY}`)
             const data = await res.json();
             console.log(data);
             setnews(data.articles)
         }
         get();
-    }, [])
+    }, [search])
     return(
         <>
+        <input type="text" value={search} onChange={(e) => setsearch(e.target.value)}/>
         <div className="container">
             {news.map((article, index) => (
                 <div key={index}>
