e2d90e2117592f52f97ade5652f25edc720be08a
Tammyboy23 <fagboyeguntamilore43@gmail.com>
2026-05-05 02:58:06 +0100

    Now showing the news information

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,19 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Page(){
+    const [news, setnews] = useState([])
     useEffect(() => {
         const get = async() => {
             const API_KEY = import.meta.env.VITE_API_KEY;
             const res = await
-            fetch(`https://newsapi.org/v2/everything?q=tesla&from=2026-04-05&sortBy=publishedAt&apiKey=${API_KEY}`)
+            fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
             const data = await res.json();
             console.log(data);
+            setnews(data.articles)
         }
         get();
     }, [])
     return(
         <>
-        <h1>Hello there</h1>
+        <div className="container">
+            {news.map((article, index) => (
+                <div key={index}>
+                    <img src={article.urlToImage} alt="" />
+                    <h1>{article.description}</h1>
+                    <p>{article.content}</p>
+                    <h5>{article.source.name}</h5>
+                </div>
+            ))}
+        </div>
         </>
     )
 }
