21c0e45309dff9e7deb2a9ae7376185bc43e2fac
Tammyboy23 <fagboyeguntamilore43@gmail.com>
2026-05-05 02:35:56 +0100

    Hidden APIkey and server works just fine noe

--- src/Page.jsx
+++ src/Page.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 function Page(){
     useEffect(() => {
         const get = async() => {
-            const API_KEY = "247b726c211c47eba644541d21179282";
+            const API_KEY = import.meta.env.VITE_API_KEY;
             const res = await
             fetch(`https://newsapi.org/v2/everything?q=tesla&from=2026-04-05&sortBy=publishedAt&apiKey=${API_KEY}`)
             const data = await res.json();
